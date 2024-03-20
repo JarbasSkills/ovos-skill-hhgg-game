f71a60c5ca11edaa1e414c5eadc989e49321f44e
miro <jarbasai@mailfence.com>
2024-03-20 19:09:56 +0000

    modernize

--- __init__.py
+++ __init__.py
@@ -1,12 +1,13 @@
 from ovos_workshop.decorators import conversational_intent, intent_handler
 from pyfrotz.ovos import FrotzSkill
-
+from pyfrotz.parsers import hhgg_intro_parser
 
 class HHGGSkill(FrotzSkill):
     def __init__(self, *args, **kwargs):
         # game is english only, apply bidirectional translation
         super().__init__(game_id="hhgg",
                          game_lang="en-us",
+                         intro_parser=hhgg_intro_parser,
                          game_data=f'{self.root_dir}/res/{self.game_id}.z5',
                          *args, **kwargs)
 
